ffde9f2ef4de03412d5535accb7582bac8bbf7d2
Xwilarg <xwilarg@protonmail.com>
2026-05-11 22:38:38 +0200

    Add input submission

--- frontend/src/main/MainForm.tsx
+++ frontend/src/main/MainForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 interface GameData
 {
@@ -11,14 +11,26 @@ interface GameWordData
     length: number
 }
 
+interface WordData
+{
+    foundIndexed: number[]
+}
+
 export default function MainForm() {
     let [data, setData] = useState<GameData | null>(null);
+    let [input, setInput] = useState("");
+    let [canType, setCanType] = useState(true);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         fetch("/api/info")
         .then(x => x.json())
         .then(x => setData(x));
     }, []);
+
+    useEffect(() => {
+        if (canType) inputRef?.current?.focus();
+    }, [ canType ])
 
     if (!data) {
         return (
@@ -31,7 +43,29 @@ export default function MainForm() {
     return (
         <>
             <div className="container box">
-                <input type="text" />
+                <input ref={inputRef} disabled={!canType} value={input} onChange={x => setInput((x.target as HTMLInputElement).value)} type="text" onKeyDown={e => {
+                    if (e.key === "Enter")
+                    {
+                        setCanType(false)
+                        fetch(`/api/validate/${input}`)
+                        .then(x => {
+                            if (x.ok) return x.json();
+                            throw new Error();
+                        })
+                        .then((x: WordData) => {
+                            setData(d => {
+                                let tokens = [...d!.tokens];
+                                for (let i of x.foundIndexed) {
+                                    tokens[i].displayedWord = input;
+                                }
+                                return { tokens: tokens };
+                            })
+                            setInput("");
+                            setCanType(true);
+                        })
+                        .catch(_ => setCanType(true));
+                    }
+                }} />
             </div>
             <div className="container box is-flex" id="word-list">
                 {
@@ -39,7 +73,7 @@ export default function MainForm() {
                         if (x.displayedWord === null)
                         {
                             return <span className="hidden-word" style={{
-                                width: (x.length * 20) + "px"
+                                width: (x.length * 14.4025) + "px"
                             }}></span>
                         }
                         return <span>{x.displayedWord}</span>
