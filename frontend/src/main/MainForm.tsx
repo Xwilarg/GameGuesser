e8704d7b2d193c4cb006bc0a5f761f236fa06e73
Xwilarg <xwilarg@protonmail.com>
2026-05-11 22:11:01 +0200

    Add frontend

--- frontend/src/main/MainForm.tsx
+++ frontend/src/main/MainForm.tsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react"
 
 interface GameData
 {
-    name: string,
-    description: string,
-    iteration: number
+    tokens: GameWordData[]
+}
+
+interface GameWordData
+{
+    displayedWord: string
+    length: number
 }
 
 export default function MainForm() {
     let [data, setData] = useState<GameData | null>(null);
 
     useEffect(() => {
-        fetch("/php/getData.php")
+        fetch("/api/info")
         .then(x => x.json())
         .then(x => setData(x));
     }, []);
@@ -25,8 +29,23 @@ export default function MainForm() {
     }
 
     return (
-        <div className="container box">
-            Data loaded...
-        </div>
+        <>
+            <div className="container box">
+                <input type="text" />
+            </div>
+            <div className="container box is-flex" id="word-list">
+                {
+                    data.tokens.map(x => {
+                        if (x.displayedWord === null)
+                        {
+                            return <span className="hidden-word" style={{
+                                width: (x.length * 20) + "px"
+                            }}></span>
+                        }
+                        return <span>{x.displayedWord}</span>
+                    })
+                }
+            </div>
+        </>
     )
 }
