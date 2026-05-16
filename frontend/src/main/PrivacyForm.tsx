93bfceaedcc4d3c3899361c7d1880daca15e2b66
Xwilarg <xwilarg@protonmail.com>
2026-05-16 23:22:09 +0200

    Add about section

--- frontend/src/main/PrivacyForm.tsx
+++ frontend/src/main/PrivacyForm.tsx
@@ -5,6 +5,9 @@ export default function PrivacyForm() {
         <div className="container box">
             <br/>
             <Link to="/" className="button">Back to game</Link><br/>
+            <h2>About</h2>
+            Game inspired by <a href="https://www.synoptix.fr/" target="_blank">Synoptix</a><br/>
+            Daily reset happens at midnight UTC+0
             <h2>Privacy</h2>
             This website is using Shika for its analytics<br/>
             You can see all the data collected <a href="https://astylodon.org/docs/shika/data" target='_blank'>here</a><br/>
