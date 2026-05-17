1323a7aabcb47fc6696b59d7bc6d813b00411c54
Xwilarg <xwilarg@protonmail.com>
2026-05-17 16:24:01 +0200

    Improve share

--- frontend/src/form/WinningForm.tsx
+++ frontend/src/form/WinningForm.tsx
@@ -9,11 +9,21 @@ interface WinningFormProps {
 function getCompletion(state: GameData) {
     let found = 0.0;
     for (let token of state.description.filter(x => x.needToBeGuessed)) {
-        if (token.displayAsClose !== null) found += 0.5;
+        if (token.displayAsClose !== null) found += token.displayAsClose;
         else if (token.displayedWord !== null) found += 1;
     }
 
     return found / state.description.filter(x => x.needToBeGuessed).length * 100.0;
+}
+
+function getCompletionEmotes(state: GameData) {
+    let str = "";
+    for (let token of state.description.filter(x => x.needToBeGuessed)) {
+        if (token.displayAsClose !== null) str += "🟩";
+        else if (token.displayedWord !== null) str += "🟧";
+        else str += "⬛";
+    }
+    return str;
 }
 
 export default function WinningForm({ state, close }: WinningFormProps) {
@@ -26,7 +36,7 @@ export default function WinningForm({ state, close }: WinningFormProps) {
             <h1>You finished the {getGameName()} #{state.iteration}</h1>
             <div className="is-flex">
                 <button onClick={() => {
-                    let shareText = `${getGameName()} #${state.iteration}\nCompletion: ${getCompletion(state).toFixed(1)}%\n\n${window.location}`;
+                    let shareText = `${getGameName()} #${state.iteration}\nCompletion: ${getCompletion(state).toFixed(1)}%\n${getCompletionEmotes(state)}\n\n${window.location}`;
                     window.prompt("Copy to share", shareText)
                 }}>Share</button>
                 <button onClick={close}>Close</button>
